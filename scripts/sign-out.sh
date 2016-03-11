1093b7b7d3481ef549559480f61b4dbfa4c40499
ajordon <airjordon2@gmail.com>
2016-03-11 15:30:28 -0500

    Test curl scripts for success Removed "username" field from User model. Left for future development.

--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --include --request DELETE http://localhost:3000/sign-out/$ID \
-  --header "Authorization: Token token=$TOKEN"
+curl --include --request DELETE http://localhost:3000/sign-out/56e3284d2715bf055a0c6f8e \
+  --header "Authorization: Token token=7NqODEMmU8rrzduLijXuug=="
--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-curl --include --request POST http://localhost:3000/sign-up \
+curl --silent --request POST http://localhost:3000/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "an@example.email",
-      "username": "weber",
-      "password": "an example password",
-      "password_confirmation": "an example password"
+      "email": "an@example.com",
+      "password": "123",
+      "password_confirmation": "123"
     }
   }'
 
-curl --include --request POST http://localhost:3000/sign-up \
-  --header "Content-Type: application/json" \
-  --data '{
-    "credentials": {
-      "email": "another@example.email",
-      "username": another_user,
-      "password": "an example password",
-      "password_confirmation": "an example password"
-    }
-  }'
+# curl --include --request POST http://localhost:3000/sign-up \
+#   --header "Content-Type: application/json" \
+#   --data '{
+#     "credentials": {
+#       "email": "another@example.email",
+#       "username": another_user,
+#       "password": "an example password",
+#       "password_confirmation": "an example password"
+#     }
+#   }'
