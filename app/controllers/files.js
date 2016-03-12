c41be51262fa96f3fbf86a8b2d0039c8e9455add
Celena T <chibiynm@gmail.com>
2016-03-12 16:40:10 -0500

    Add files controller, npm install multer

--- app/controllers/files.js
+++ app/controllers/files.js
@@ -5,10 +5,11 @@ const models = require('app/models');
 const File = models.file;
 const awsS3Upload = require('../../bin/aws-upload');
 
-// const authenticate = require('./concerns/authenticate');
+const authenticate = require('./concerns/authenticate');
 
 // multer for uploading
 const multer = require('multer'); // Antony had require('./concerns/multer.js') but it crashed nodemon
+
 const upload = multer({ storage: multer.memoryStorage() });
 
 const index = (req, res, next) => {
