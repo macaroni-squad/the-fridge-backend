6481ea64aa994d8696465e75e314c158ab2c0d4f
Celena T <chibiynm@gmail.com>
2016-03-12 17:13:59 -0500

    Add changes related to FormData workaround

--- app/controllers/files.js
+++ app/controllers/files.js
@@ -8,6 +8,7 @@ const awsS3Upload = require('../../bin/aws-upload');
 const authenticate = require('./concerns/authenticate');
 
 // multer for uploading
+
 const multer = require('multer'); // Antony had require('./concerns/multer.js') but it crashed nodemon
 
 const upload = multer({ storage: multer.memoryStorage() });
