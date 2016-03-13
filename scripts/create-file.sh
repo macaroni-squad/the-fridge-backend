1d3a360e00fc3e78178f3f1b14bc8436329fad0f
JenWeber <weberj10@gmail.com>
2016-03-13 19:40:47 -0400

    fixed create script

--- scripts/create-file.sh
+++ scripts/create-file.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# this curl script doesn't work, I'm actually not sure how to test this
+# works. Don't forget to use file and not fileSSSS. Run this with bash scripts/create-file.sh and make sure you have a file to upload in your scripts folder
 
 curl --silent --request POST http://localhost:3000/files \
 --header "Content-Type: application/json" \
 --data '{
-  "files": {
-    "fileType": "jpg",
-    "location": "../../tmp/pusheen-in-fridge.jpg"
-    "description": "Pusheen in fridge"
+  "file": {
+    "title": "dumplings",
+    "location": "./dumplings.jpg",
+    "description": "Pusheen in fridge",
+    "_owner": "56e5f14ee095e3ae538a637d"
   }
 }'
