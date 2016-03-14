a3298f9433163818f451e9aa5012f93da1485faa
mfbrewster <michael.f.brewster@gmail.com>
2016-03-13 20:06:38 -0400

    making file  controller neater

--- app/controllers/files.js
+++ app/controllers/files.js
@@ -7,9 +7,7 @@ const File = models.file;
 const authenticate = require('./concerns/authenticate');
 
 // multer for uploading
-
 const multer = require('multer'); // Antony had require('./concerns/multer.js') but it crashed nodemon
-
 const upload = multer({ storage: multer.memoryStorage() });
 
 const index = (req, res, next) => {
