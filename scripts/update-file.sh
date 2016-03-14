6b68668474b06b892b23478a78bb6046b33e79ac
JenWeber <weberj10@gmail.com>
2016-03-14 11:27:02 -0400

    post to s3 works but not mongo

--- scripts/update-file.sh
+++ scripts/update-file.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-curl --include --request PATCH http://localhost:3000/files/56e5f4f1d19fd51d166e431a \
-  --header "Authorization: Token token=heW0X8I5hhkBas7ziBOalA==" \
+# this script works
+
+curl --include --request PATCH http://localhost:3000/files/56e5f78f2b96d59c5607bc48 \
+  --header "Authorization: Token token=sNS+7j1qEtebMUH/CvxJtA==" \
   --header "Content-Type: application/json" \
-  --data '{
-      "file": {
-        "title": "The title has been edited",
-        "description": "This file has been edited"
-      }
+--data '{
+    "file": {
+      "description": "Pusheen is NOT in the fridge"
+    }
   }'
